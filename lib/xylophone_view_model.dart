7d3dc41353a7da12438af0aa2f5a2270b8ce2284
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-06-29 12:40:44 +0900

    코드 리뷰 실로폰

--- lib/xylophone_view_model.dart
+++ lib/xylophone_view_model.dart
@@ -3,69 +3,69 @@ import 'package:flutter/services.dart';
 import 'package:soundpool/soundpool.dart';
 
 class XylophoneViewModel {
-  final Soundpool pool =
+  final Soundpool _pool =
       Soundpool.fromOptions(options: SoundpoolOptions.kDefault);
-  final List<int> soundIds = [];
-  final StreamController<bool> _isLoadingController = StreamController<bool>();
+  final List<int> _soundIds = [];
 
-  Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
-  void init() {
-    _loadSounds();
-  }
-
-  Future<void> _loadSounds() async {
-    _isLoadingController.add(true);
+  Future<void> loadSounds() async {
     int soundId = await rootBundle
         .load('assets/do1.wav')
-        .then((soundData) => pool.load(soundData));
+        .then((soundData) => _pool.load(soundData));
 
-    soundIds.add(soundId);
+    _soundIds.add(soundId);
     soundId = await rootBundle
         .load('assets/re.wav')
-        .then((soundData) => pool.load(soundData));
+        .then((soundData) => _pool.load(soundData));
 
-    soundIds.add(soundId);
+    _soundIds.add(soundId);
     soundId = await rootBundle
         .load('assets/mi.wav')
-        .then((soundData) => pool.load(soundData));
+        .then((soundData) => _pool.load(soundData));
 
-    soundIds.add(soundId);
+    _soundIds.add(soundId);
     soundId = await rootBundle
         .load('assets/fa.wav')
-        .then((soundData) => pool.load(soundData));
+        .then((soundData) => _pool.load(soundData));
 
-    soundIds.add(soundId);
+    _soundIds.add(soundId);
     soundId = await rootBundle
         .load('assets/sol.wav')
-        .then((soundData) => pool.load(soundData));
+        .then((soundData) => _pool.load(soundData));
 
-    soundIds.add(soundId);
+    _soundIds.add(soundId);
     soundId = await rootBundle
         .load('assets/la.wav')
-        .then((soundData) => pool.load(soundData));
+        .then((soundData) => _pool.load(soundData));
 
-    soundIds.add(soundId);
+    _soundIds.add(soundId);
     soundId = await rootBundle
         .load('assets/si.wav')
-        .then((soundData) => pool.load(soundData));
+        .then((soundData) => _pool.load(soundData));
 
-    soundIds.add(soundId);
+    _soundIds.add(soundId);
     soundId = await rootBundle
         .load('assets/do2.wav')
-        .then((soundData) => pool.load(soundData));
+        .then((soundData) => _pool.load(soundData));
 
-    soundIds.add(soundId);
-
-    _isLoadingController.add(false);
+    _soundIds.add(soundId);
   }
 
-  void playSound(int soundId) {
-    pool.play(soundId);
+  void playDo() {
+    _playSound(_soundIds[0]);
+  }
+  void playRe() {
+    _playSound(_soundIds[1]);
+  }
+  void playMi() {
+    _playSound(_soundIds[2]);
+  }
+
+  void _playSound(int soundId) {
+    _pool.play(soundId);
   }
 
   void dispose() {
-    pool.dispose();
-    _isLoadingController.close();
+    _pool.dispose();
   }
 }
